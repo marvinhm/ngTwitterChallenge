ee5e0cc1fafa3db29e0f820019dce9598fed0b3f
Marvin Matovu <marvinmatovu@gmail.com>
2019-11-05 19:06:51 +0000

    redirect to the index page when we click on submit

--- cypress/integration/welcome_page.spec.js
+++ cypress/integration/welcome_page.spec.js
@@ -4,24 +4,10 @@ describe("Testing the twitter homepage", () => {
     cy.contains("Welcome To Twitter");
   })
 
-  it("Should contain a form title", () => {
-    cy.visit('http://localhost:4200');
-    cy.contains("Login");
-  })
-
-  it("Should contain username input title", () => {
-    cy.visit('http://localhost:4200');
-    cy.contains("username");
-  })
 
   it("Should contain username input element", () => {
     cy.visit('http://localhost:4200');
     cy.get('[data-test="usernameInput"]').should('exist');
-  })
-
-  it("Should contain password input title", () => {
-    cy.visit('http://localhost:4200');
-    cy.contains("password");
   })
 
   it("Should contain password input element", () => {
