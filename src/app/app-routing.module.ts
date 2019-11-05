a6fceb18f8a6bdd2ec5454a5c1dfe7ff262bdc3b
Marvin Matovu <marvinmatovu@gmail.com>
2019-11-05 19:28:25 +0000

    configure routes for login, index

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { TweetsComponent} from './components/tweets/tweets.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'login', component: LoginComponent},
+  {path: 'index', component: TweetsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import { IndexComponent } from './components/index/index.component';
+import { TweetsComponent } from './components/tweets/tweets.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HeaderComponent
+    HeaderComponent,
+    IndexComponent,
+    TweetsComponent
   ],
   imports: [
     BrowserModule,
